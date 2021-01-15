a2a41f7c5c3e34385318e4b1e99e9a85617779c9
DmitriFa <lipsuke@gmail.com>
2021-01-15 11:11:41 +0200

    15.01.21

---
+++ src/main/java/com/example/demoboot/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.demoboot.repository;
+
+import com.example.demoboot.entitiy.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByLastName(String lastName);
+
+    boolean existsDistinctByLastName(String lastname);
+
+
+
+}
