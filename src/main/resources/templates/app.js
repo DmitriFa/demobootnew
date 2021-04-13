9a72c8438e18e4b1764a59de0305dfd9570a9170
DmitriFa <lipsuke@gmail.com>
2021-04-13 14:52:39 +0300

    13.04.21

---
+++ src/main/resources/templates/app.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    if (document.getElementById("myCheck").checked) {
+        document.getElementById("myCheck").value="1";
+    }
+    else {
+        document.getElementById("myCheck").value="0";
+    }
+}
+
+function myFunction1() {
+    if (document.getElementById("myCheck1").checked) {
+        document.getElementById("myCheck1").value="1";
+    }
+    else {
+        document.getElementById("myCheck1").value="0";
+    }
+}
+//$(document).ready(function() {
+ //   $("a").click(function() {
+  //      alert("Hello world!");
+  //  });
+//});
